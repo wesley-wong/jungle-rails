09c2d88cad8d95688a9f892247adb2e152de167c
trrippy <wes.wong@hotmail.com>
2017-02-16 01:14:20 +0000

    queries db and sends to page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,13 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @products = Product.all.order(created_at: :desc)
+  end
+
+  def new
+    @category = Category.all
+  end
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
