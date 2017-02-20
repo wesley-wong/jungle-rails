be971f255dcb600b2ae29405bb8d6c4dd66687b6
trrippy <wes.wong@hotmail.com>
2017-02-20 21:50:02 +0000

    deleted this because rspec

--- test/mailers/previews/invoice_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/example_mailer
-class InvoiceMailerPreview < ActionMailer::Preview
-
-  def invoice_mail_preview
-    InvoiceMailer.invoice_email(Order.last)
-  end
-
-end
