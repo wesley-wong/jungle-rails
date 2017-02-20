56ff25f6fd15acb018a9901b493e7d3e041f685d
trrippy <wes.wong@hotmail.com>
2017-02-20 23:12:54 +0000

    added minimum length for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, :uniqueness => {:case_sensitive => false}
-  validates :password, presence: true, confirmation: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 3 }
   validates :password_confirmation, presence: true
 end
