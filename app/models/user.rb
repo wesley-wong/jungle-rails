f5c04a0d0a67cb1236d4a7dfa694b46e3b03fc16
trrippy <wes.wong@hotmail.com>
2017-02-20 21:47:21 +0000

    updated validation to make email case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, :uniqueness => {:case_sensitive => false}
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
 end
